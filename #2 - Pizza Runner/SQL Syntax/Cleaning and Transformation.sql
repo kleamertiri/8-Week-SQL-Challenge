6593692bbd84a80be89f8bcbaa4808f97693bd81
Klea Mertiri <105167291+kleamertiri@users.noreply.github.com>
2023-06-19 16:53:23 +0200

    Add files via upload

---
+++ #2 - Pizza Runner/SQL Syntax/Cleaning and Transformation.sql
@@ -0,0 +1,55 @@
+-------------------------------------
+---Data Cleaning & Transformation----
+-------------------------------------
+
+
+-- runners_orders table(cleaning + transformation + temp table)
+DROP TABLE IF EXISTS #TEMP_runners_orders;
+SELECT order_id, runner_id, 
+CASE 
+	WHEN pickup_time IS NULL OR pickup_time = 'null' THEN ''
+	ELSE pickup_time
+	END AS pickup_time,
+CASE
+	WHEN distance = 'null' THEN ''
+	WHEN distance LIKE '%km' THEN TRIM('km' FROM distance)
+	ELSE distance
+	END AS distance,
+CASE
+	WHEN duration LIKE '%mins' THEN TRIM('mins' FROM duration)
+	WHEN duration LIKE '%minutes' THEN TRIM('minutes' FROM duration)
+	WHEN duration LIKE '%minute' THEN TRIM('minute' FROM duration)
+	WHEN duration = 'null' THEN ''
+	ELSE duration
+	END AS duration,
+CASE
+	WHEN cancellation IS NULL OR cancellation = 'null' THEN ''
+	ELSE cancellation
+	END AS cancellation
+INTO #TEMP_runners_orders
+FROM runner_orders;
+
+SELECT * FROM #TEMP_runners_orders;
+
+
+-- customer_orders table(cleaning + transformation + temp table)
+DROP TABLE IF EXISTS #TEMP_customer_orders;
+SELECT order_id, customer_id, pizza_id, 
+CASE
+	WHEN exclusions = 'null' OR exclusions IS NULL THEN ''
+	ELSE exclusions
+	END AS exclusions,
+CASE
+	WHEN extras = 'null' OR extras IS NULL THEN ''
+	ELSE extras
+	END AS extras,
+order_time
+INTO #TEMP_customer_orders
+FROM customer_orders;
+
+SELECT * FROM #TEMP_customer_orders;
+
+
+
+
+
